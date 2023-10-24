6c1410653724b8e3d4cea0c70d64eacdc35dab9d
Victor Arruda <victor0070000@gmail.com>
2023-10-24 19:41:00 -0300

    feat: add page to show category products

--- src/app/(home)/components/category-item.tsx
+++ src/app/(home)/components/category-item.tsx
@@ -1,33 +1,17 @@
 import { Badge } from "@/components/ui/badge";
+import { CATEGORY_ICON } from "@/constants/category-icon";
 import { Category } from "@prisma/client";
-import {
-  HeadphonesIcon,
-  KeyboardIcon,
-  MonitorIcon,
-  MouseIcon,
-  SpeakerIcon,
-  SquareDotIcon,
-} from "lucide-react";
-
 interface CategoryItemProps {
   category: Category;
 }
 
 const CategoryItem = ({ category }: CategoryItemProps) => {
-  const categoryIcon = {
-    keyboards: <KeyboardIcon size={16} />,
-    monitors: <MonitorIcon size={16} />,
-    headphones: <HeadphonesIcon size={16} />,
-    mousepads: <SquareDotIcon size={16} />,
-    speakers: <SpeakerIcon size={16} />,
-    mouses: <MouseIcon size={16} />,
-  };
   return (
     <Badge
       variant="outline"
       className="flex items-center justify-center gap-7 rounded-lg py-3"
     >
-      {categoryIcon[category.slug as keyof typeof categoryIcon]}
+      {CATEGORY_ICON[category.slug as keyof typeof CATEGORY_ICON]}
       <span className="text-xs font-semibold">{category.name}</span>
     </Badge>
   );
---
+++ src/constants/category-icon.tsx
@@ -0,0 +1,17 @@
+import {
+  HeadphonesIcon,
+  KeyboardIcon,
+  MonitorIcon,
+  MouseIcon,
+  SpeakerIcon,
+  SquareDotIcon,
+} from "lucide-react";
+
+export const CATEGORY_ICON = {
+  keyboards: <KeyboardIcon size={16} />,
+  monitors: <MonitorIcon size={16} />,
+  headphones: <HeadphonesIcon size={16} />,
+  mousepads: <SquareDotIcon size={16} />,
+  speakers: <SpeakerIcon size={16} />,
+  mouses: <MouseIcon size={16} />,
+};
